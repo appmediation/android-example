c5c309cf726a0b09f3b3e1eb9bb27868ba8841c2
Aleksandr Chatsky <aleksandr@appmediation.com>
2018-02-20 11:58:52 +0200

    Changed app key and app package. Added listener examples.

--- app/src/main/java/com/appmediation/demo_android/MainActivity.java
+++ app/src/main/java/com/appmediation/demo_android/MainActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.appmediation.sdk.AMBanner;
 import com.appmediation.sdk.AMInterstitial;
 import com.appmediation.sdk.AMRewardedVideo;
 import com.appmediation.sdk.AMSDK;
+import com.appmediation.sdk.listeners.AMBannerListener;
+import com.appmediation.sdk.listeners.AMListener;
+import com.appmediation.sdk.listeners.AMRewardedListener;
+import com.appmediation.sdk.models.AMError;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,12 +21,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AMSDK.init(this, "4ccbacd3-5ddf-4884-bbe8-ca6f1feba482");
+        AMSDK.init(this, "a1cdd0c4-de3b-421f-a7b3-5c264c16df91");
 
         findViewById(R.id.interstitialButton).setOnClickListener(this);
         findViewById(R.id.rewardedVideoButton).setOnClickListener(this);
-        findViewById(R.id.showBannerButton).setOnClickListener(this);
-        findViewById(R.id.hideBannerButton).setOnClickListener(this);
+
+        setNewBannerListener();
+        setNewInterstitialListener();
+        setNewRewardedListener();
     }
 
     @Override
@@ -42,5 +49,97 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             default:
                 return;
         }
+    }
+
+    private void showMessage(String text) {
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    private void setNewBannerListener() {
+        AMBanner.setListener(new AMBannerListener() {
+            @Override
+            public void onLoaded() {
+                showMessage("Banner ad: onLoaded");
+            }
+
+            @Override
+            public void onFailed(AMError error) {
+                showMessage("Banner ad: onFailed: " + error.name());
+            }
+
+            @Override
+            public void onShowed() {
+                showMessage("Banner ad: onShowed");
+            }
+
+            @Override
+            public void onClicked() {
+                showMessage("Banner ad: onClicked");
+            }
+        });
+    }
+
+    private void setNewInterstitialListener() {
+        AMInterstitial.setListener(new AMListener() {
+            @Override
+            public void onLoaded() {
+                showMessage("Interstitial ad: onLoaded");
+            }
+            @Override
+            public void onFailed(AMError error) {
+                showMessage("Interstitial ad: onFailed: " + error.name());
+            }
+
+            @Override
+            public void onShowed() {
+                showMessage("Interstitial ad: onShowed");
+            }
+
+            @Override
+            public void onClosed() {
+                showMessage("Interstitial ad: onClosed");
+            }
+
+            @Override
+            public void onClicked() {
+                showMessage("Interstitial ad: onClicked");
+            }
+        });
+    }
+
+    private void setNewRewardedListener() {
+        AMRewardedVideo.setListener(new AMRewardedListener() {
+            @Override
+            public void onLoaded(String currency, String amount) {
+                String rewardInfo = amount + " " + currency;
+                showMessage("Rewarded ad: onLoaded: " + rewardInfo);
+            }
+
+            @Override
+            public void onCompleted(String currency, String amount) {
+                String rewardInfo = amount + " " + currency;
+                showMessage("Rewarded ad: onCompleted: " + rewardInfo);
+            }
+
+            @Override
+            public void onFailed(AMError error) {
+                showMessage("Rewarded ad: onFailed: " + error.name());
+            }
+
+            @Override
+            public void onShowed() {
+                showMessage("Rewarded ad: onShowed");
+            }
+
+            @Override
+            public void onClosed() {
+                showMessage("Rewarded ad: onClosed");
+            }
+
+            @Override
+            public void onClicked() {
+                showMessage("Rewarded ad: onClicked");
+            }
+        });
     }
 }
